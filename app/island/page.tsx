834f0872bc0ceb43e8d7eae15e4e2788e15c427d
mikephayashi <mikephayashi@gmail.com>
2026-01-19 14:39:22 -0800

    foundation

---
+++ app/island/page.tsx
@@ -0,0 +1,9 @@
+export default function IslandPage() {
+  return (
+    <main
+      className="min-h-screen w-full bg-cover bg-center"
+      style={{ backgroundImage: "url('/island-background.png')" }}
+    />
+  );
+}
+
